659c7470072b12020670ee2f6d7d280c37e1daf9
Lisa McGerr <lisamarie.mcgerr@gmail.com>
2021-01-20 12:22:59 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :artists, :songs
+  resources :artists, :songs, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+drake = Artist.create(name: "Drake")
+ben_bohmer = Artist.create(name: "Beh Bohmer")
+
+greece = Song.create(title: "GREECE", artist_id: 1)
+laugh_now = Song.create(title: "Laugh Now Cry Later", artist_id: 1)
+gods_plan = Song.create(title: "Gods's Plan", artist_id: 1)
+cappadocia = Song.create(title: "Cappadocia", artist_id: 2)
